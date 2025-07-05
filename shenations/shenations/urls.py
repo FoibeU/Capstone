6164d21b53e0013344c23e9afc1773d5d2ed9521
Foibe RWATANGABO <f.uwizeyima@alustudent.com>
2025-07-06 01:00:45 +0200

    connect

--- shenations/shenations/urls.py
+++ shenations/shenations/urls.py
@@ -1,15 +1,14 @@
-
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
-
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
+def redirect_to_frontend(request):
+    return redirect('https://capstone-kohl-chi.vercel.app/')  # Your frontend URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),   
-    path('api/auth/', include('accounts.urls')), 
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('accounts.urls')),
     path('api/courses/', include('courses.urls')),
     path('api/applications/', include('applications.urls')),
+    path('', redirect_to_frontend),  # Redirect root URL to frontend
 ]
